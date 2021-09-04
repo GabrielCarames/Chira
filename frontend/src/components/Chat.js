d385f10f7841420d708269384e1fa178e1858f43
Gabriel Carames <49535753+GabrielCarames@users.noreply.github.com>
2021-09-04 10:23:37 -0300

    Auto scroll chat, send message on enter, word break fixed and some bugs corrected

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,9 +1,11 @@
 import avatar from '../images/avatar.png'
 import socket from './Socket'
 import { useState, useEffect } from 'react'
+import ReactScrolleableFeed from 'react-scrollable-feed'
 
 const Chat = () => {
     const [messages, setMessages] = useState("");
+    const [inputMessage, setInputMessage] = useState("")
     const user = JSON.parse(localStorage.getItem('userLogged'))
     const { _id, username, phoneNumber } = user
 
@@ -27,6 +29,25 @@ const Chat = () => {
         // setMensaje("");
     };
 
+    useEffect(() => {
+        const listener = event => {
+            if (event.code === "Enter" || event.code === "NumpadEnter") {
+                const input = document.getElementsByClassName('main__input');
+                if (input[0] === document.activeElement) {
+                    event.preventDefault();
+                    if(inputMessage!== '') {
+                        messageInput(inputMessage)
+                        input[0].value = ''
+                    }
+                }
+            }
+        };
+        document.addEventListener("keydown", listener);
+        return () => {
+            document.removeEventListener("keydown", listener);
+        };
+      }, [inputMessage]);
+
     return(
         <section className="main__chat-section">
                 <nav className="main__chat-navbar navbar">
@@ -48,31 +69,31 @@ const Chat = () => {
                     </div>
                 </nav>
                 <div className="main__messages-section messages">
+                <ReactScrolleableFeed>
                     {messages && messages.map((message) => {
                         return (
                             message.username === user.username ?
-                                <div className="cosa">
-
+                                <div className="messages-user-logged-messages">
+                                    <div className="messages-message-container">
+                                        <span className="messages__username">{message.username}</span>
+                                        <p className="messages__message">{message.message}</p>
+                                        <h6 className="messages__timeago">30:43hs</h6>
+                                    </div>
+                                </div>
+                            : 
+                            <div className="messages-contact-messages">
                                 <div className="messages-message-container">
                                     <span className="messages__username">{message.username}</span>
                                     <p className="messages__message">{message.message}</p>
                                     <h6 className="messages__timeago">30:43hs</h6>
                                 </div>
-                                </div>
-                            : 
-                            <div className="cosa-malvada">
-
-                            <div className="messages-message-container">
-                                <span className="messages__username">{message.username}</span>
-                                <p className="messages__message">{message.message}</p>
-                                <h6 className="messages__timeago">30:43hs</h6>
-                            </div>
                             </div>
                         )
                     })}
+                    </ReactScrolleableFeed>
                 </div>
-                <div className="main__input-section">
-                    <input className="main__input" type="text" name="message" id="" placeholder="Escribe un mensaje aquí" autoComplete="off" onChange={(e) => messageInput(e.target.value)} />
+                <div className="main__input-section" >
+                    <input className="main__input" id="cosa" type="text" name="message" id="" placeholder="Escribe un mensaje aquí" autoComplete="off" onChange={(e) => setInputMessage(e.target.value)} />
                 </div>
             </section>
     )
