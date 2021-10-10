04ca5813950b8fb40dece57e0b95355a252dcc8e
Gabriel Carames <gabrielcarames1@gmail.com>
2021-10-10 13:25:15 -0300

    Inverted chat messages location and edit profile avatar issue solved

--- frontend/src/components/DisplayMessages.js
+++ frontend/src/components/DisplayMessages.js
@@ -7,26 +7,26 @@ const DisplayMessages = (({message, user, goToMessage, scrollToBottom, setImages
 
     return (
         message.user.username === user.username || message.username === user.username ?
-        <div className={goToMessage === message._id ? 'messages-user-logged-messages active' : 'messages-user-logged-messages'} key={message._id} id={message._id}>
-            <div className="messages-message-container">
-                <span className="messages__username">{message.user.username}</span>
-                {displayMessage(message)}
-                <div className="message__info">
-                    <h6 className="messages__timeago">{moment(message.createdAt).format('LT')}</h6>
-                    <DisplaySeenIcon message={message}/>
+            <div className={goToMessage === message._id ? 'messages-contact-messages active' : 'messages-contact-messages'} key={message._id} id={message._id}>
+                <div className="messages-message-container">
+                    <span className="messages__username">{message.user.username}</span>
+                    {displayMessage(message)}
+                    <div className="message__info">
+                        <h6 className="messages__timeago">{moment(message.createdAt).format('LT')}</h6>
+                    </div>
                 </div>
             </div>
-        </div>
         : 
-        <div className={goToMessage === message._id ? 'messages-contact-messages active' : 'messages-contact-messages'} key={message._id} id={message._id}>
-            <div className="messages-message-container">
-                <span className="messages__username">{message.user.username}</span>
-                {displayMessage(message)}
-                <div className="message__info">
-                    <h6 className="messages__timeago">{moment(message.createdAt).format('LT')}</h6>
+            <div className={goToMessage === message._id ? 'messages-user-logged-messages active' : 'messages-user-logged-messages'} key={message._id} id={message._id}>
+                <div className="messages-message-container">
+                    <span className="messages__username">{message.user.username}</span>
+                    {displayMessage(message)}
+                    <div className="message__info">
+                        <h6 className="messages__timeago">{moment(message.createdAt).format('LT')}</h6>
+                        <DisplaySeenIcon message={message}/>
+                    </div>
                 </div>
             </div>
-        </div>
     )
 })
 
